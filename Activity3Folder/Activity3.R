3a2e3717f6a4ae99332455b0d114534d7ca677f3
cjachowicz <cjachowicz@colgate.edu>
2025-09-23 11:10:25 -0400

    Measurement outside of sensor capabilities script (Q4-6)

--- Activity3Folder/Activity3.R
+++ Activity3Folder/Activity3.R
@@ -108,18 +108,42 @@ plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
 datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
 #new col <- ifelse(if temperature < 0, replace the value with NA, otherwise keep its current temp)
 
+#check the values at the extreme range of the data (each quartile)
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+#sometimes removing extremes is good, but you have to be cautious
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,] 
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,]  
 
+#############Question 4#############
 
+#plot precipitation and lightning strikes on the same plot
+#normalize lighting strikes to match precipitation
+lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+#make the plot with precipitation and lightning activity marked
+#make it empty to start and add in features
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
+     type="n")
+#plot precipitation points only when there is precipitation 
+#make the points semi-transparent
+points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
+       col= rgb(95/255,158/255,160/255,.5), pch=15)        
 
+#plot lightning points only when there is lightning     
+points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
+       col= "tomato3", pch=19)
 
+#############Question 5#############
 
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
+#create a new air temp column
+datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
 
-
-
-
-
-
-
+#############Question 6#############
 
 
 
