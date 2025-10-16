bca03b44a4c5afaf08408499394ed55679e6932e
cjachowicz <cjachowicz@colgate.edu>
2025-10-16 11:12:36 -0400

    wrote script for part 2 and 3

--- Activity4Folder/Activity4.R
+++ Activity4Folder/Activity4.R
@@ -5,8 +5,12 @@
 #take a look at it 
 head(iris)
 #load in some tidyverse packages
+dplyr::filter()
+dplyr::lag()
 library(tidyverse)
-
+#mask every conflict with dplyr packages
+dplyr::filter()
+dplyr::lag()
 #####################################
 ##### Part 1: for loops         #####
 #####################################
@@ -36,13 +40,13 @@ forlooplist <- list(
     iris_petal_length ~ iris_petal_width,
     iris_sepal_length ~ iris_petal_length
 )
-#for loop creating every regression table
+#for-loop creating every regression table
 for (duet in forlooplist){
-  
   model <- lm(duet, data = iris)
+  print("Model Summary: ")
+  print(duet)
   summary(model)
 }
-
 #####################################
 ##### Part 2: data in dplyr     #####
 #####################################
@@ -52,7 +56,11 @@ for (duet in forlooplist){
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
-filter(iris.data, ..., preserve = FALSE)
+virginica <- iris[iris$Species == "virginica",]
+setosa <- iris[iris$Species == "setosa",]
+versicolor <- iris[iris$Species == "versicolor",]
+
+filter(iris, setosa$Sepal.Length < 100)
 
 #####################################
 ##### Part 3: plots in ggplot2  #####
@@ -62,16 +70,32 @@ filter(iris.data, ..., preserve = FALSE)
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
-e <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width))
+new_plot <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width))
 
 #3b. make a scatter plot with ggplot and get rid of  busy grid lines
-e + theme_classic() + geom_point()
+new_plot + theme_classic() + geom_point()
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
+fancy <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Length))
+
+fancy + labs(x = "Petal length (cm)",
+                  y = "Petal width (cm)",
+                  title = "Petal length and width by species")
+fancy + scale_radius()
+
+fancy + theme_classic() + geom_point()
+
+fancy <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = "red", size = Petal.Length))
+fancy + theme_classic() + geom_point() + scale_color_manual(values = c("red" = "turquoise2"))
+
+
+
+
 
 #####################################
 ##### Question: how did         #####
 ##### arguments differ between  #####
 ##### plot and ggplot?          #####
 #####################################		
+
